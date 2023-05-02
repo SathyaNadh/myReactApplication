9a12ce05e44324c1096bd4c3b17196ad2695d4f0
Your Name <you@example.com>
2023-05-02 13:06:25 +0530

    moblie menu changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,16 +2,48 @@ import React ,{useState,useEffect} from 'react'
 import Link from 'next/link'
 export const Menu = () =>{ 
 
-  const [menuitem,setMenuItem]= useState('home')
+  const [menuitem,setMenuItem]= useState('')
+  const [ismobliemenu,setIsMoblieMenu]=useState(false)
+  const [left,setLeft]=useState(-150)
+
+  const getBrowserWidth=()=>{
+    let timeoutId;
+    let flag=true
+    setIsMoblieMenu(document.body.offsetWidth< 600 ? true :false)
+    window.addEventListener('resize',()=>{
+      let width= document.body.offsetWidth
+      if(flag){
+        flag=false;
+        setIsMoblieMenu(width< 600 ? true :false)
+      }
+      clearTimeout(timeoutId)
+      timeoutId= setTimeout(() => {
+        setIsMoblieMenu(width< 600 ? true :false)
+      }, 1000);
+    })
+  }
+  useEffect(()=>{
+    getBrowserWidth()
+  },[])
+
+  
    useEffect(()=>{
     let pathName=window.location.pathname
     setMenuItem(pathName. slice(1) || 'home')
   },[])
   const fnClick=(event)=>{
   setMenuItem(event.target.id)
+  if(ismobliemenu){
+    setLeft(-150)
+  }
+ }
+ const fnMoblieMenuBtnClick=()=>{
+  setLeft(left === 0 ? -150 :0)
  }
   return (
-    <ul className='menu'>
+    <div>
+      {ismobliemenu && <button  onClick={fnMoblieMenuBtnClick} className='moblie-menu-btn'>menu</button>}
+    <ul style={{left:left}} className={ismobliemenu ? 'moblie-menu' :'menu'}>
       <li>
         <Link  id='home' className={menuitem == 'home'&& 'active-menu'} onClick={fnClick}href="/home">Home</Link>
       </li>
@@ -22,6 +54,7 @@ export const Menu = () =>{
         <Link  id='contact' className={menuitem == 'contact'&& 'active-menu'}onClick={fnClick} href="/contact">Contact</Link>
       </li>
     </ul>
+    </div>
   )
 }
 
